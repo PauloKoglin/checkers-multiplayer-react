83c2464e463b2eacecfd0cd5435c156573174746
paulo.koglin <pk.koglin@gmail.com>
2020-08-07 07:55:55 +0200

    server address change

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
-// const server_URL = process.env.SERVER_URL || 'https://checkers-multiplayer-server.herokuapp.com' || 'http://localhost:3001';
-const server_URL = 'http://localhost:3001';
+const server_URL = process.env.SERVER_URL || 'https://checkers-multiplayer-server.herokuapp.com' || 'http://localhost:3001';
+// const server_URL = 'http://localhost:3001';
 
 console.log('Definied api server: ' + server_URL);
 console.log(process.env.SERVER_URL);
